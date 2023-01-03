244a4aac9b2d8dd884760052d8fa4db135f41b4b
midooth2010 <81426884+midooth2010@users.noreply.github.com>
2023-01-03 21:44:22 +0200

    Initial commit

---
+++ src/Chapter2/GrossPayCalculator.java
@@ -0,0 +1,13 @@
+package Chapter2;
+
+import java.util.Scanner;
+
+public class GrossPayCalculator {
+public static void main (String[] args){
+     Scanner input = new Scanner(System.in);
+     System.out.println("Enter your Name");
+     input.next();
+
+
+}
+}
